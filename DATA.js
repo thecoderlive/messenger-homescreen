14afeaab67d2fe4908c8a571624f23fd62b1a1ca
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-30 20:03:20 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,62 @@
+export const item = {
+    "id": "a4f20f2d-951f-4ced-a288-1f1ec2c0c2be",
+    "stories": [
+        {
+            "id": "367be0e4-91fa-40ff-a727-a995fa356aba",
+            "story_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png",
+            "name": "million"
+        },
+        {
+            "id": "686c591b-9345-4719-b288-a52f29ad63a4",
+            "story_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png",
+            "name": "much"
+        },
+        {
+            "id": "c505cfb4-29ca-43e1-8bea-76620f710181",
+            "story_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png",
+            "name": "cut"
+        },
+        {
+            "id": "688b9bd8-b21e-45e9-910a-28074f6e0b37",
+            "story_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png",
+            "name": "seem"
+        },
+        {
+            "id": "c43616a4-a40c-47cc-905d-d597ad042736",
+            "story_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png",
+            "name": "office"
+        }
+    ],
+    "messages": [
+        {
+            "id": "e356c436-928a-41ad-92af-a17de8c15c88",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png",
+            "friend_name": "quickly",
+            "last_message": "try"
+        },
+        {
+            "id": "7230fed2-3b59-4020-8565-e90ecf1306c1",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png",
+            "friend_name": "lawyer",
+            "last_message": "simply"
+        },
+        {
+            "id": "ebc215db-ff15-4b89-9b34-4f1987dd7562",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png",
+            "friend_name": "like",
+            "last_message": "act"
+        },
+        {
+            "id": "c7d72482-e2a2-49ec-a653-fd75268bb06d",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png",
+            "friend_name": "second",
+            "last_message": "bank"
+        },
+        {
+            "id": "b782767e-e550-4ed4-97a9-cf4bf035ba00",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/148.png",
+            "friend_name": "draw",
+            "last_message": "tree"
+        }
+    ]
+}
